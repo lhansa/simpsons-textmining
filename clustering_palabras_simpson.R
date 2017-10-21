41302599276606b093341a13478400c02e645c8f
Leonardo Torres <leonardo.torres@conento.com>
2017-10-22 00:47:18 +0200

    cluster y wordcloud

--- clustering_palabras_simpson.R
+++ clustering_palabras_simpson.R
@@ -3,6 +3,10 @@ rm(list=ls()); gc()
 library(tidyverse)
 library(subtools)
 library(tidytext)
+library(glue)
+library(wordcloud)
+
+## Datos ----------------------------------------------
 
 df <- read.subtitles.serie(dir = "./The Simpsons/") %>%
   subDataFrame() %>%
@@ -26,8 +30,8 @@ df <- df %>%
   group_by(season_episode) %>%
   count(word)
 
-df$word %>% unique %>% length
-df$word %>% unique %>% sort %>% range
+# df$word %>% unique %>% length
+# df$word %>% unique %>% sort %>% range
 
 ## Filtramos palabras que empiecen por dígitos
 
@@ -44,7 +48,6 @@ for (var in names(df_pivot)) {
   df_pivot[[var]][rlang::are_na(df_pivot[[var]])] <- 0
 }
 
-df_pivot
 
 
 ## Componentes principales ----------------------------------------------------
@@ -56,7 +59,45 @@ summary(componentes)
 
 df_componentes <- componentes$x %>% 
   as_tibble() %>% 
-  select(PC1:PC30) %>% 
-  mutate(season_episode = df_pivot$season_episode)
+  select(PC1:PC30)
 
-df_componentes
+rm(componentes); gc()
+
+
+## k-means ---------------------------------------------------------
+
+nombres_componentes <- names(df_componentes)
+
+cortes <- function(varr){
+  quantile(varr, probs = seq(0,1, by = 0.2))
+}
+
+dots <- map(nombres_componentes, function(var){
+    as.formula(glue("~findInterval({var}, vec = cortes({var}))"))
+  })
+
+set.seed(31818)
+clobjeto <- df_componentes %>% 
+  mutate_(.dots = set_names(dots, nombres_componentes)) %>% 
+  kmeans(centers = 4)
+
+## Exploración de resultados ---------------------------------------
+
+df_pivot %>% 
+  mutate(cluster = clobjeto$cluster) %>% 
+  count(cluster)
+
+df <- df_pivot %>% 
+  mutate(cluster = clobjeto$cluster) %>% 
+  gather("word","conteo",-season_episode, -cluster)
+
+df %>% 
+  arrange(cluster, desc(conteo))
+
+## Wordcloud --------------------------------------------------------
+
+aux <- df %>% 
+  filter(cluster == 1)
+  
+
+wordcloud(aux$word, aux$conteo)
